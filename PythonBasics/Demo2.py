754e492768296e936609340704a284d2293f1d9f
Swati <swatibabaria1@gmail.com>
2023-10-25 19:20:23 +0530

    American guy's update

--- PythonBasics/Demo2.py
+++ PythonBasics/Demo2.py
@@ -1,5 +1,5 @@
 
-values = [1, 2, "rahul", 4, 5, 6, "shetty"]
+values = [1, 2, "rahul", 4, 5, 6, "shetty", 7, 8]
 # List is data type that allows multiple values and can be different data types
 
 print(values[0])  # 1
