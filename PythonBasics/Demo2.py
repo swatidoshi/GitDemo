4182eff33679f8948500432a0cae87f7ace9c12e
Swati <swatibabaria1@gmail.com>
2023-10-25 20:51:26 +0530

    American development team

--- PythonBasics/Demo2.py
+++ PythonBasics/Demo2.py
@@ -7,7 +7,7 @@ print(values[0])  # 1
 print(values[3])  # 4
 print(values[-1])  #5
 print(values[1:3])  # 2 rahul
-values.insert(3, "shetty")   #[1, 2, 'rahul', 'shetty', 4, 5]
+values.insert(3, "Swati")   #[1, 2, 'rahul', 'shetty', 4, 5]
 print(values)
 values.append("End")  #[1, 2, 'rahul', 'shetty', 4, 5, 'End']
 print(values)
