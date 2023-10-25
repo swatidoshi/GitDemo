f549dc2fe597081a9e3b4b1590eb3cf7f7249f17
Swati <swatibabaria1@gmail.com>
2023-10-25 20:34:32 +0530

    Asian architect development

--- PythonBasics/ChildImp.py
+++ PythonBasics/ChildImp.py
@@ -5,7 +5,7 @@ class ChildImpl(Calculator):
     num2 = 200
 
     def __init__(self):
-        Calculator.__init__(self, 2, 10)
+        Calculator.__init__(self, 2, 8)
 
     def getCompleteData(self):
         return self.num2 + self.num + self.Summation()
--- PythonBasics/FirstDemo.py
+++ PythonBasics/FirstDemo.py
@@ -1,1 +1,1 @@
-print('hello')
+print('hello! How are you?')
